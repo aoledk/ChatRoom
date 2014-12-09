4eb308209064e4b9fc73898574b0cb44e3c7e6da
linux1993 <dingkang1743@gmail.com>
2014-12-09 12:42:00 +0800

    ＂rd＂

---
+++ public/js/room.js
@@ -0,0 +1,27 @@
+// JS for room
+
+
+function appInit() {
+	console.log(username);
+	$('p').text('body load');
+}
+
+//$(document).ready(function() {
+
+  	//console.log(username);
+  	//console.log(roomname);
+
+  	//if (username === 'LDK')
+    	//$('p').text('LDK');
+  	//else
+    	//$('p').text('OTHER');
+
+  	/*easyrtc.setRoomOccupantListener(callEverybodyElse);
+  	easyrtc.easyApp('chatRoom', 'box0', ['box1', 'box2'. 'box3'], loginSuccess);
+  	easyrtc.setPeerListener(messageListener);
+  	easyrtc.setDisconnectListener(function() {
+  		easyrtc.showError('LOST-CONNECTION', 'Lost connection to signaling server');
+  	});*
+});
+
+
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-app.use(express.static(__dirname + "/static/"));
+app.use(express.static(__dirname + "/public/"));
 
 app.get('/', function(req, res) {
 	res.render('index');
