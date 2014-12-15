b3f40f377a6bde1b35d2dac8674018ab3bf311b8
linux1993 <dingkang1743@gmail.com>
2014-12-15 11:35:53 +0800

    Adjust form in index

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,10 @@
 // JS for index
+
+$('#submit').click(function() {
+  var user = $('#username').val();
+  var room = $('#roomname').val();
+  if (user == '' || room == '') {
+  	$('#login_msg').val('Empty username OR Empty roomname');
+  	window.location.replace('/');
+  }
+});
